996173e39e6d2b4c1baf045fab1f82732aa0db6d
DanielOliverThomson@gmail.com <DanielOliverThomson@gmail.com>
2023-06-26 14:28:55 -0600

    Removing stray logging

--- math/differentiation/mjs/staticTeX.mjs
+++ math/differentiation/mjs/staticTeX.mjs
@@ -6,11 +6,6 @@ document.querySelectorAll('.staticTeX').forEach(
   el => {
     let origTeX = el.querySelector('span.origTeX');
     let outputTeX = document.createElement('div');
-    console.log(origTeX);
-    console.log();
-    console.log(outputTeX);
-
-
     outputTeX.className = 'TeXbox';
     // outputTeX.setHTML(katex.renderToString(origTeX.innerText));
     outputTeX.innerHTML = katex.renderToString(origTeX.innerText);
