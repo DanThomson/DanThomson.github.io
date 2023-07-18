9a11d3ac10c86986c7945335fb1d1d2511eea980
DanielOliverThomson@gmail.com <DanielOliverThomson@gmail.com>
2023-07-17 18:30:58 -0600

    Much better turtle graphics

--- svg/mjs/turtle.mjs
+++ svg/mjs/turtle.mjs
@@ -1,21 +1,72 @@
 // Top level turtle graphics module
 
-import movement from 'movement';
 import controls from 'controls';
+// import movement from 'movement';
 
 
-// Radians * (180 / Math.PI) = degrees
-const rtod = 180 / Math.PI;
-// Degrees * (Math.PI / 180) = radians
-const dtor = Math.PI / 180;
+function step_forward(path_d, turtle_direction, step_length){
+  // let path_d = turtle_path.getAttribute('d');
+  let dy, dx;
+
+  dy = step_length * Math.sin(turtle_direction);
+  dx = step_length * Math.cos(turtle_direction);
+
+  return `l ${dx} ${dy} `;
+}
 
 
-let turtle_path = document.querySelector('#turtle_path');
+function drawPath (turtle_path, commands) {
+  const step_length = controls.get_step_length();
+  const turn_angle = controls.get_turn_angle();
+  let path_d = 'M 5 5 ';  // Start in top left corner
+  let turtle_direction = Math.PI / 2;  // Start facing down
+
+  for (let c of commands) {
+    if (c === 'F') {
+      // F -> step_forward
+      path_d += step_forward(path_d, turtle_direction, step_length);
+    } else if (c === '-') {
+      // - -> turn right
+      turtle_direction -= turn_angle;
+    } else if (c === '+') {
+      // + -> turn left
+      turtle_direction += turn_angle;
+    } else {
+      // Report bad character!
+      console.log('Unknown character in commands string:');
+      console.log(c);
+      console.log('Found in:');
+      console.log(commands);
+    }
+  }
+
+  turtle_path.setAttribute('d', path_d);
+}
+
+// Pattern p = Pattern.compile("[,. ]+");
+// // repeat only the following part:
+// String output = p.matcher(input).replaceAll("");
+
+function rewrite_commands (commands, f_replacement) {
+  if (commands === '') {
+    return controls.get_initial_phrase();
+  }
+  return commands.replaceAll('F', f_replacement)
+}
 
 
-// Inital state
-let forward_increment = 10;
-let turtle_direction = Math.PI / 2;  // Start facing up
-
-
-movement.step_forward(turtle_path, turtle_direction, forward_increment);
+// Add listener to update button to perform the update when the button is 'clicked'
+controls.update_c.addEventListener(
+  'click',
+  e => {
+    const f_replacement = controls.get_f_replacement();
+    let commands = '';
+    controls.get_figures().forEach(
+      (figure) => {
+        const turtle_path = figure.querySelector('.turtle_path');
+        commands = rewrite_commands(commands, f_replacement);
+        drawPath(turtle_path, commands);
+      }
+    );
+  }
+);
--- svg/mjs/turtle_controls.mjs
+++ svg/mjs/turtle_controls.mjs
@@ -1,22 +1,53 @@
 // Get controls
 // Set listeners
 
+// Radians * (180 / Math.PI) = degrees
+const rtod = 180 / Math.PI;  // Not currently used
+// Degrees * (Math.PI / 180) = radians
+const dtor = Math.PI / 180;
 
 
 const controls_node = document.querySelector('#controls');
-const gen_count = controls_node.querySelector('#gen_count');
-gen_count.focus();  // for debugging / testing I want the focus there
+const update_c = controls_node.querySelector('#update');
 
-const step_length = controls_node.querySelector('#step_length');
-const initial_phrase = controls_node.querySelector('#initial_phrase');
-const f_replacement = controls_node.querySelector('#f_replacement');
-// let x = controls_node.querySelector('x')
+
+const gen_count_c = controls_node.querySelector('#gen_count');
+function get_gen_count () {
+  return Number(gen_count_c.value);
+}
+
+
+const step_length_c = controls_node.querySelector('#step_length');
+function get_step_length () {
+  return Number(step_length_c.value);
+}
+
+
+const turn_angle_c = controls_node.querySelector('#turn_angle');
+function get_turn_angle () {
+  return Number(turn_angle_c.value) * dtor;
+}
+
+
+const initial_phrase_c = controls_node.querySelector('#initial_phrase');
+function get_initial_phrase () {
+  return initial_phrase_c.value;
+}
+
+
+const f_replacement_c = controls_node.querySelector('#f_replacement');
+function get_f_replacement () {
+  return f_replacement_c.value;
+}
 
 
 const figures = document.querySelector('#figures');
-document.figures = figures;
+function get_figures () {
+  return Array.prototype.slice.call( figures.children );
+}
 
-gen_count.addEventListener('change',
+
+gen_count_c.addEventListener('change',
   change_event => {
     const count = change_event.srcElement.value;
     // TODO: validation
@@ -44,6 +75,30 @@ gen_count.addEventListener('change',
 );
 
 
+// Generic update event. This must come after the update for
+// gen_count because that event creates and destroys html elements
+// which will be the target of code that reacts to a control's
+// update. When a control is updated we simulate update_c.click,
+// so dependent code can react to any control's change by adding a
+// click event handler to update_c.
+function onEvent (e) {
+  update_c.click();
+}
+const controls = [gen_count_c, step_length_c, turn_angle_c, initial_phrase_c, f_replacement_c];
+controls.forEach(
+  control => {
+    control.addEventListener('change', onEvent);
+  }
+);
 
-const name_space = {  };
+
+const name_space = {
+  update_c,
+  get_figures,
+  get_gen_count,
+  get_step_length,
+  get_turn_angle,
+  get_initial_phrase,
+  get_f_replacement,
+};
 export default name_space;
